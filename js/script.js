a710a15ff209f61fe6e102cb7031243274fe3892
Gavrilova_Tanya <tanya.belova19@yandex.ru>
2021-10-20 23:23:54 +0300

    Функциональность побита на методы. Реалиовано добавление нового фильма по подтверждению и удаление фильма по клику на иконку

--- js/script.js
+++ js/script.js
@@ -19,20 +19,17 @@ document.addEventListener('DOMContentLoaded', () =>{
         genre = document.querySelector('.promo__genre'),
         newBackground = document.querySelector('.promo__bg'),
         btn = document.querySelector('button'),
-        addForm = document.querySelector('form.add');
-    let removeElements = document.querySelectorAll('.delete'),
+        addForm = document.querySelector('form.add'),
         inputMovie = addForm.querySelector('.adding__input'),
         checkbox = addForm.querySelector('[type="checkbox"]');
 
     // Удаление всех рекламных блоков со страницы (правая часть сайта)
     
-    function deleteAdvertising(){
-        advertising.forEach(item =>{
+    const deleteAdvertising = (array) =>{
+        array.forEach(item =>{
             item.remove();
         });
-    }
-
-    deleteAdvertising();
+    };
 
     // advertising.forEach(function(item){
     //     item.remove();
@@ -40,20 +37,21 @@ document.addEventListener('DOMContentLoaded', () =>{
     
     // Изменение жанра фильма с "комедия" на "драма"
 
-    function changeGenre(newGenre){
+    const changePage = (genre, newGenre)=> {
         genre.textContent = newGenre;
-    }
+        newBackground.style.backgroundImage = 'url("../img/bg.jpg")';
+    };
+    
 
-    changeGenre('Драма');
+    const sortArray = (array) =>{
+        array.sort();
+    };
     
-    // Изменение заднего фона постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
-    newBackground.style.backgroundImage = 'url("../img/bg.jpg")';
-    // newBackground.style.background = "url(../img/bg.jpg) center center/cover no-repeat";
-    
-    // Метод для сортировки и добавления порядковых номеров фильмов
-    function addList(films, parent)
+    // Метод добавления порядковых номеров фильмов
+    function addOrderedMovieList(films, parent)
     {        
         parent.innerHTML = "";
+        sortArray(movieDB.movies);
         
         films.forEach((film, i) => {
             parent.innerHTML += `
@@ -62,60 +60,62 @@ document.addEventListener('DOMContentLoaded', () =>{
                 </li>
             `;
         });
+
+        document.querySelectorAll('.delete').forEach((rmElement, i)=>{
+            rmElement.addEventListener('click', () => {
+                rmElement.parentElement.remove();
+                films.splice(i, 1);
+                addOrderedMovieList(films, parent);
+            });
+        });
+
     }
-    
-    addList(movieDB.movies, movs);
     
     addForm.addEventListener('submit', (e) => {
     
         e.preventDefault(); 
     
         let newMovie = inputMovie.value;
-        let favoriteMovie = checkbox.checked;
+        const favoriteMovie = checkbox.checked;
 
-        if (newMovie == ''){
-            alert('Введите название фильма!');
-            return;
-        }
+        if (newMovie){
+            if (newMovie.length > 21){
+                newMovie = `${newMovie.substr(0,21)}...`;
+            }
+        
+            movieDB.movies.push(newMovie);
+            sortArray(movieDB.movies);
     
-        if (newMovie.length > 21)
-        {
-            newMovie = String(newMovie).substr(0,21) + "...";
-        }
-    
-        movieDB.movies.push(newMovie);
-
-        movieDB.movies.sort();
-
-        addList(movieDB.movies, movs);
-
-        inputMovie.value = "";
+            addOrderedMovieList(movieDB.movies, movs);
+                
+            if(favoriteMovie){
+                console.log('Добавлен любимый фильм');
+            }
             
-        if(favoriteMovie)
-        {
-            console.log('Добавлен любимый фильм');
+            e.target.reset();
         }
-        checkbox.checked=false;
+
      });
     
+    // Неверный путь
+    //  const deleteElement = (e) => {
+    //     e.preventDefault();
+    //     const removeMovie = e.target.parentNode.textContent[0];
+    //     console.log(removeMovie);
+    //     e.target.parentNode.remove();
+    //     movieDB.movies.splice(removeMovie-1,1);
+    //     addOrderedMovieList(movieDB.movies, movs);
+    // };
     
-     const deleteElement = (e) => {
-        e.preventDefault();
-        const removeMovie = e.target.parentNode.textContent[0];
-        console.log(removeMovie);
-        e.target.parentNode.remove();
-        movieDB.movies.splice(removeMovie-1,1);
-        addList(movieDB.movies, movs);
-    };
+    // function addListener(){
+    //     removeElements.forEach(element => {
+    //         element.addEventListener('click', deleteElement);
+    //     });
+    // }
     
-    function addListener(){
-        removeElements.forEach(element => {
-            element.addEventListener('click', deleteElement);
-        });
-    }
-    
-    addListener();
-
+    deleteAdvertising(advertising);
+    changePage(genre,'Драма');
+    addOrderedMovieList(movieDB.movies, movs);
 });
 
 
